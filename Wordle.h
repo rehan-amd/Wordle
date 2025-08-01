86def214a02acebc95c77ce4ce1acbda90070a00
rehan-amd <rehanahmad45580@gmail.com>
2025-08-01 09:55:08 -0700

    Add Wordle.h

---
+++ Wordle.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <random>
+using namespace std;
+
+extern const int WORD_SIZE;
+extern string WORD;
+
+extern string roundRecord;
+extern string gameRecord[5];
+
+void gameStart();
+
+void setRandomWordFromFile();
+
+void centreOutput();
+void displayTitle();
+void displayInstructions();
+
+bool isLetterInWord(char);
+bool isEnglishWord(string);
+
+void takeInput(string&, int);
+bool isMatch(string);
+void displayWord(string, int);
+
+
+void displayThanks();
+void showRecord();
+void GameWin();
+void GameLost();
