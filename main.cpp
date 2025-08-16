92cf17479a938c5c7d43b47485ec0a94111a9119
rehan-amd <rehanahmad45580@gmail.com>
2025-08-16 16:06:44 +0500

    Update main.cpp
    Improved, Simplified Version

--- main.cpp
+++ main.cpp
@@ -1,40 +1,8 @@
 #include "wordle.h"
 
-using namespace std;
-
 int main()
 {
-	try
-	{
-		gameStart();
-	}
-	catch (string e)
-	{
-		cout << e;
-		return 0;
-	}
-
-	string guess;
-	int attempts = 5;
-	while (attempts != 0)
-	{
-		takeInput(guess, attempts);
-
-		displayWord(guess, attempts);
-
-		system("pause");
-		system("cls");
-
-		if (isMatch(guess))
-		{
-			GameWin();
-			return 0;
-		}
-
-		attempts--;
-	}
-
-	GameLost();
+	GAME();
 
 	return 0;
 }
